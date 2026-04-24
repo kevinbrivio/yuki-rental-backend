9786d0da44e169b23184e83d594f00e384eb8e73
itzKv <kenkenku6@gmail.com>
2026-04-24 07:30:00 +0700

    add: server app

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,2 +1,24 @@
 package server
 
+import (
+	"log/slog"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/kevinbrivio/yuki-rental-backend/internal/config"
+)
+
+type Server struct {
+	cfg *config.Config
+	db *pgxpool.Pool
+	log *slog.Logger
+}
+
+func New(cfg *config.Config, db *pgxpool.Pool, log *slog.Logger) *Server {
+	log.Info("Server created")
+	return &Server{
+		cfg: cfg,
+		db: db,
+		log: log,
+	}
+}
