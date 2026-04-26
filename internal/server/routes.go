f85e67e87a5c3cf3cfb9b60a3e9cce14ed6de58d
itzKv <kenkenku6@gmail.com>
2026-04-26 14:14:52 +0700

    add: recovery middleware

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -10,24 +10,29 @@ import (
 
 func (s *Server) routes() chi.Router {
 	r := chi.NewRouter()
-	
+
 	// middleware goes here
 	r.Use(middleware.Logger(s.log))
-	
+	r.Use(middleware.Recovery(s.log))
+
 	// router goes here
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		
-		// Check db
-		err := s.db.Ping(r.Context())
-		if err != nil {
-			w.WriteHeader(http.StatusServiceUnavailable)
-			json.NewEncoder(w).Encode(map[string]string{"status": "ok", "database": "down"})
-		} else {
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(map[string]string{"status": "ok", "database": "up"})
-		}
-	})
-	
+	r.Get("/health", s.handleHealth)
+
 	return r
+}
+
+func (s *Server) handleHealth(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	
+	w.Header().Set("Content-Type", "application/json")
+
+	// Check db
+	err := s.db.Ping(ctx)
+	if err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		json.NewEncoder(w).Encode(map[string]string{"status": "error", "database": "down"})
+	} else {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok", "database": "up"})
+	}
 }
