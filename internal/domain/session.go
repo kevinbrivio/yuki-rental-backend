10ea8f2598950d91629dbdd6f5c47d5c44442a92
itzKv <kenkenku6@gmail.com>
2026-04-29 18:06:48 +0700

    update: gorm model

--- internal/domain/session.go
+++ internal/domain/session.go
@@ -3,9 +3,9 @@ package domain
 import "time"
 
 type Session struct {
-	ID string `json:"id,omitempty"`
-	UserID string `json:"user_id"`
-	TokenHash string `json:"-"`
+	ID string `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id,omitempty"`
+	UserID string `gorm:"type:uuid;not null" json:"user_id"`
+	TokenHash string `gorm:"not null" json:"-"`
 	IPAddress string `json:"ip_address"`
 	UserAgent string `json:"user_agent"`
 	ExpiresAt time.Time `json:"expires_at"`
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,6 +1,10 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type UserRole string
 const (
@@ -15,18 +19,18 @@ const (
 )
 
 type User struct {
-	ID string `json:"id,omitempty"`
-	Email string `json:"email"`
-	PasswordHash string `json:"-"`
-	PhoneNumber string `json:"phone_number"`
-	Role UserRole `json:"role"`
+	ID string `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id,omitempty"`
+	Email string `gorm:"uniqueIndex;not null" json:"email"`
+	PasswordHash string `gorm:"not null" json:"-"`
+	PhoneNumber string `gorm:"uniqueIndex;not null" json:"phone_number"`
+	Role UserRole `gorm:"type:user_role;not null" json:"role"`
 	FirstName string `json:"first_name"`
 	LastName string `json:"last_name"`
 	DateOfBirth time.Time `json:"date_of_birth"`
-	Gender GenderType `json:"gender"`
+	Gender GenderType `gorm:"type:gender_type;not null" json:"gender"`
 	AvatarURL string `json:"avatar_url"`
 	IsActive bool `json:"is_active"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
-	DeletedAt *time.Time `json:"deleted_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
